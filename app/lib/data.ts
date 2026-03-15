3d2539b3125e8a5d067729c899fa0d1414f51b92
Tholey Matteo <matteo.tholey@gmail.com>
2026-03-15 20:17:14 +0100

    ajout dashboard revenue chart

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,7 +11,8 @@ import { formatCurrency } from './utils';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
-export async function fetchRevenue() {
+export async function fetchRevenue() 
+{
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
