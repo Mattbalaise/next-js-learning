95214867b238a5821c9f58b9c85e9a957e3a6f60
Tholey Matteo <matteo.tholey@gmail.com>
2026-03-21 14:09:35 +0100

    feat : skeleton / data fetching / ...

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,12 +17,10 @@ export async function fetchRevenue()
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -39,7 +37,7 @@ export async function fetchLatestInvoices() {
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
-
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     const latestInvoices = data.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
